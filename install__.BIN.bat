63a863dcd0e146b7b5f9841afdcbd13d830afb4f
aymaneallaoui <aymanepraxe@gmail.com>
2023-07-22 15:25:22 +0000

    fix

--- install__.BIN.bat
+++ install__.BIN.bat
@@ -1,42 +1,29 @@
 @echo off
-
-REM Check if the script is running with administrative privileges
-NET SESSION >nul 2>&1
-if %errorlevel% neq 0 (
-    echo This script requires administrative privileges. Please run as an administrator.
-    pause
-    exit /b 1
-)
-
 setlocal
 
-REM Set the URL for the latest fzf release
-set "fzf_release_url=https://github.com/junegunn/fzf/releases/latest"
+REM Define the download URL for fzf
+set "fzfVersion=0.42.0"
+set "fzfZipFileName=fzf-%fzfVersion%-windows_amd64.zip"
+set "fzfDownloadURL=https://github.com/junegunn/fzf/releases/download/%fzfVersion%/%fzfZipFileName%"
 
-REM Set the target installation directory for fzf binary (change as needed)
-set "installation_directory=C:\Windows\System32"
+REM Set the target directory where fzf binary will be placed
+set "targetDirectory=C:\Windows\System32"
 
-REM Download the latest fzf release using curl (you can replace with other download tools)
-echo Downloading fzf binary...
-curl -sSL -o fzf.zip %fzf_release_url%
+REM Download the fzf zip file
+echo Downloading fzf...
+curl -L -o "%fzfZipFileName%" "%fzfDownloadURL%"
 
-REM Unzip the downloaded archive
-echo Extracting fzf binary...
-powershell -command "Expand-Archive -Path .\fzf.zip -DestinationPath .\fzf"
+REM Unzip the fzf binary from the downloaded zip file
+echo Extracting fzf...
+powershell -Command "Expand-Archive -Path '%fzfZipFileName%' -DestinationPath '.' -Force"
 
-REM Move the fzf binary to the installation directory
-echo Moving fzf binary to %installation_directory%...
-move /y .\fzf\fzf.exe "%installation_directory%"
+REM Move the fzf binary to the target directory
+echo Moving fzf binary to %targetDirectory%...
+move fzf.exe "%targetDirectory%\fzf.exe" > nul
 
-REM Clean up temporary files
+REM Clean up the downloaded zip file
 echo Cleaning up...
-rmdir /s /q .\fzf
-del fzf.zip
+del "%fzfZipFileName%"
 
-REM Add the installation directory to the PATH environment variable
-echo Updating PATH...
-setx PATH "%installation_directory%;%PATH%" /M
-
-echo Installation completed successfully!
-
+echo fzf has been installed successfully to %targetDirectory%\fzf.exe
 pause
