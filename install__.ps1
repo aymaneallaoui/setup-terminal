6716315c30684a4ba70d2c1feddb7b1a9d0e7cdf
aymaneallaoui <aymanepraxe@gmail.com>
2023-07-22 15:17:31 +0000

    install url fix

--- install__.ps1
+++ install__.ps1
@@ -1,4 +1,4 @@
-$downloadUrl = "https://objects.githubusercontent.com/github-production-release-asset-2e65be/13807606/4c46be1c-6cbd-4a91-8f91-4d56250ef7e9?X-Amz-Algorithm=AWS4-HMAC-SHA256&X-Amz-Credential=AKIAIWNJYAX4CSVEH53A%2F20230722%2Fus-east-1%2Fs3%2Faws4_request&X-Amz-Date=20230722T140528Z&X-Amz-Expires=300&X-Amz-Signature=a7333ab0a193e2b41c524d810a7c6cf52ec25487aba845bbdec71f66750c9642&X-Amz-SignedHeaders=host&actor_id=111997551&key_id=0&repo_id=13807606&response-content-disposition=attachment%3B%20filename%3Dfzf-0.42.0-windows_amd64.zip&response-content-type=application%2Foctet-stream"
+$downloadUrl = "https://github.com/junegunn/fzf/releases/download/0.42.0/fzf-0.42.0-windows_amd64.zip"
 $downloadFolder = "$env:TEMP\fzf_download"
 
 # Function to download and extract fzf
